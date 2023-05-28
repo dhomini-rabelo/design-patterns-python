6ae329f8fc91bc73edca58635a3f234bc327eb98
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2023-05-28 15:53:01 -0300

    add IDateManager

---
+++ src/patterns/AbstractFactory/implementations/main/dependencies/country_factory/contract/dependencies/date_manager/main.py
@@ -0,0 +1,12 @@
+from abc import ABC as AbstractFactory, abstractmethod
+from datetime import datetime
+
+
+class IDateManager(AbstractFactory):
+    @abstractmethod
+    def get_representation(self, datetime_obj: datetime) -> str:
+        pass
+
+    @abstractmethod
+    def get_datetime(self) -> datetime:
+        pass
